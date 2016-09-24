2e32321f8c702a17125328f7964910e1b187f8f8
jpbos <jpbos@users.noreply.github.com>
2016-09-24 17:10:38 -0400

    First step Jackal motion

---
+++ test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+def test():
+  pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+  rospy.init_node('test', anonymous = True)
+  rate = rospy.Rate(10)
+  move = Twist
+
+  while not rospy.is_shutdown():
+    move.linear.x = 1.0
+    move.linear.y = 0.0
+    move.linear.z = 0.0
+    move.angular.x = 0.0
+    move.angular.y = 0.0
+    move.angular.z = 0.0
+    
+    pub.publish(move)
+    rate.sleep()
+
+if __name__ == '__main__':
+  try:
+    test()
+  except rospy.ROSInterruptException:
+    pass
+
