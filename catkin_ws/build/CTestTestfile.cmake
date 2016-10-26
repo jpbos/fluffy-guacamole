b006d1591685de70d0cec95b95ff3aded79d064e
Cameron Burke <cnburke@mtu.edu>
2016-10-26 15:03:09 -0400

    Fixed the repository to include a different structuring as to keep all development on the master branch.

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cameron/repos/fluffy-guacamole/catkin_ws/src
+# Build directory: /home/cameron/repos/fluffy-guacamole/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(letters_dev)
+SUBDIRS(simple_navigation_goals)
