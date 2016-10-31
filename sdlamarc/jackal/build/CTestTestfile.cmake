5b636513466ce99f58cefaf47bc96342c86fb737
Sam LaMarche <sdlamarc@mtu.edu>
2016-10-31 17:41:54 -0400

    Added jackal workspace

---
+++ sdlamarc/jackal/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sdlamarc/Repos/fluffy-guacamole/sdlamarc/jackal/src
+# Build directory: /home/sdlamarc/Repos/fluffy-guacamole/sdlamarc/jackal/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ sdlamarc/jackal/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sdlamarc/Repos/fluffy-guacamole/sdlamarc/jackal/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/sdlamarc/Repos/fluffy-guacamole/sdlamarc/jackal/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sdlamarc/Repos/fluffy-guacamole/sdlamarc/jackal/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/sdlamarc/Repos/fluffy-guacamole/sdlamarc/jackal/src:$ROS_PACKAGE_PATH"
