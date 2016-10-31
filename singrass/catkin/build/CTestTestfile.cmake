0276d790e37840e371cc2a48c08ac80b25df715c
Sal Ingrassia <ingrassiasal@gmail.com>
2016-10-31 16:26:21 -0400

    Added singrass dir

---
+++ singrass/catkin/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sal/Repos/fluffy-guacamole/catkin/src
+# Build directory: /home/sal/Repos/fluffy-guacamole/catkin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ singrass/catkin/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/sal/Repos/fluffy-guacamole/catkin/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/sal/Repos/fluffy-guacamole/catkin/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/sal/Repos/fluffy-guacamole/catkin/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/sal/Repos/fluffy-guacamole/catkin/src:$ROS_PACKAGE_PATH"
